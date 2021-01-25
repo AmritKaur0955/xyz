5bd983c75b044269641ff5703c30fd9ec70f7af4
BaljeetAmrit <72460561+AmritKaur0955@users.noreply.github.com>
2021-01-25 11:02:26 -0500

    Region set kiti

--- xyz/ViewController.swift
+++ xyz/ViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
-
+import MapKit
+//dsdssd
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mapView :MKMapView!
+    var coooooordinates = [CLLocationCoordinate2D]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
+        let coordinates = CLLocationCoordinate2D(latitude: 43.7315, longitude: -79.7624)
+        setRegion(coordinates: coordinates)
+       // setPin(coordinates: coordinates)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(touch:)))
+        mapView.addGestureRecognizer(tap)
+        
+    }
+    @objc func handleTap(touch : UITapGestureRecognizer){
+        let point = touch.location(in: mapView)
+        let coordinates = mapView.convert(point, toCoordinateFrom: mapView)
+        coooooordinates.append(coordinates)
+        setPin(coordinates: coordinates)
+        if coooooordinates.count == 3{
+            let polu = MKPolygon(coordinates: coooooordinates, count: coooooordinates.count)
+            mapView.addOverlay(polu)
+            
+        }
+    }
+    func setRegion(coordinates : CLLocationCoordinate2D){
+        let span = MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+        let region = MKCoordinateRegion(center: coordinates , span: span)
+        mapView.region = region
+    }
+    
+    func setPin(coordinates : CLLocationCoordinate2D){
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinates
+        pin.title = "Hello"
+        mapView.addAnnotation(pin)
     }
 
 
+}
+extension ViewController : MKMapViewDelegate{
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if let x = overlay as? MKPolygon{
+            let poly = MKPolygonRenderer(polygon: x)
+            poly.strokeColor = .blue
+            poly.lineWidth = 5
+            return poly
+        }
+        return MKOverlayRenderer(overlay: overlay)
+    }
 }
 
